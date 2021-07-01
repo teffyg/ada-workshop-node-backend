9685e29cec234eb542736479f7f74334240cf286
teffyg <teffygiordano@gmail.com>
2021-07-01 00:02:32 +0000

    movie service

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const basePath = '/';
+const db = require('./../database.json');
 
 router.get(basePath, (req, res) => {
-    res.status(200).json('Hello World');
-});
+  const filter = req.query
 
+  let result = db.filter(item => {
+      for(let key in filter){
+          if  (item[key] === undefined || item[key] != filter[key])
+          return false;
+      }
+      return true;
+  });
+  res.status(200).json(result);
+
+});
 module.exports = router;
